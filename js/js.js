2ed1370bea143507989bd13a7266a5ea15d16cf9
eSSeGH <ste.virgadaula@gmail.com>
2023-02-08 17:19:28 +0100

    JS pari e dispari ex. added

--- js/js.js
+++ js/js.js
@@ -30,19 +30,46 @@ while (lowerCasePick != "pari" && lowerCasePick != "dispari") {
     console.log(lowerCasePick)
 }
 
-let userNum = prompt("scegli un numero da 1 a 5")
-console.log(userNum)
+let userNum = parseInt(prompt("scegli un numero da 1 a 5"))
+console.log("l'utente ha scelto:", userNum)
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
-let cpuNum = 0
+let randomNum = 0
 
-randMinMax(cpuNum, 1, 5)
-console.log(randMinMax(cpuNum, 1, 5))
+let cpuNum = randMinMax(randomNum, 1, 5)
+
+console.log("Il computer sceglie:", cpuNum)
 
 // Sommiamo i due numeri
+
+let sum = userNum + cpuNum
+console.log(sum)
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+let isEvenSum = isEven(sum)
+
+if (isEvenSum = true) {
+    console.log("Il risultato è: Pari")
+} else {
+    console.log("Il risultato è: Dispari")
+}
+
 // Comunichiamo all’utente chi ha vinto. (decidete voi come)
+
+if (isEvenSum = true && lowerCasePick === "pari") {
+    console.log("HAI VINTO! :)")
+    
+} else if (isEvenSum = false && lowerCasePick === "pari") {
+    console.log("HAI PERSO! :'(")
+
+} else if (isEvenSum = true && lowerCasePick === "dispari") {
+    console.log("HAI PERSO! :'(")
+
+} else if (isEvenSum = true && lowerCasePick === "dispari") {
+    console.log("HAI VINTO! :)")
+}
 
 
 
@@ -70,4 +97,16 @@ function randMinMax(variable, min, max) {
     variable = Math.floor(Math.random() * (max - min + 1)) + min
 
     return variable
+}
+
+function isEven(number) {
+
+    let resto = number % 2
+
+    if (resto === 0){
+
+        return true
+    } else {
+        return false
+    }
 }
